e31681867bb617754a724d9f18758471a17e4c82
Bonkomprann <waymaiker@laposte.net>
2023-02-01 05:36:46 +0100

    Block the vote feature when user already voted

--- components/RegisteredVoter/VotingSessionStartedView.jsx
+++ components/RegisteredVoter/VotingSessionStartedView.jsx
@@ -1,3 +1,5 @@
+import { useAccount } from "wagmi"
+
 import AddVote from "../OnlyVoters/AddVote"
 import GetVoter from "../OnlyVoters/GetVoter"
 import VotersContent from "../Voters/VotersContent"
@@ -5,17 +7,30 @@ import VotersContent from "../Voters/VotersContent"
 import useEventsProvider from "@/hooks/useEventsProvider"
 
 import { WORKFLOW_STATUS_VALUE } from "@/utils/constants"
-import { Flex } from "@chakra-ui/react"
+import { Card, Flex, FormControl, Input, Text } from "@chakra-ui/react"
 
 export default function VotingSessionStartedView() {
-  const { workflowStatus } = useEventsProvider()
+  const { address } = useAccount()
+  const { workflowStatus, votedList } = useEventsProvider()
+  const isUserAlreadyVoted = votedList.find(vote => vote.voterAddress == address) != undefined
 
-  return WORKFLOW_STATUS_VALUE[workflowStatus.previousStatus] == "VotingSessionStarted" 
-  && <Flex direction="column" alignItems="center"> 
-      <Flex alignItems="center"> 
-        <AddVote /> 
+  return WORKFLOW_STATUS_VALUE[workflowStatus.previousStatus] == "VotingSessionStarted"
+  && <Flex direction="column" alignItems="center">
+      <Flex>
+      {
+          isUserAlreadyVoted
+          ? <Card direction="column" p="5" m="2" backgroundColor="white" borderRadius="10" variant='outline'>
+              <Flex grow="1" direction='column' justifyContent="center" alignItems="center">
+                <Text fontWeight="medium">User has alredy voted</Text>
+                <FormControl>
+                  <Input disabled  placeholder="" />
+                </FormControl>
+              </Flex>
+            </Card>
+          : <AddVote />
+        }
         <GetVoter />
-      </Flex> 
+      </Flex>
       <VotersContent />
-    </Flex> 
+    </Flex>
 }
